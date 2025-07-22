874b81038a499ca0fce88fcf988a6330f094c5c3
zac1227 <a0986710233@gmail.com>
2025-07-22 09:58:49 +0800

    Update test.py

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ def generate_codebook(df, column_types, variable_names, category_definitions, ou
         total_rows, total_cols = df.shape
         doc.add_paragraph(f"總筆數 - 資料列數: {total_rows}")
         doc.add_paragraph(f"欄位數 - 變數數量: {total_cols}")
-        doc.add_paragraph("欄位缺失值統計（僅顯示有缺失的欄位）:")
+        doc.add_paragraph("欄位缺失值統計（僅顯示有缺失的欄位）")
 
         na_series = df.isnull().sum()
         na_table = doc.add_table(rows=1, cols=3)
