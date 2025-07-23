3a159e0dcfb065d721b3e9f198c9d681f033f4ac
zac1227 <a0986710233@gmail.com>
2025-07-23 09:07:29 +0800

    Update test.py

--- test.py
+++ test.py
@@ -12,6 +12,7 @@ def generate_codebook(df, column_types, variable_names, category_definitions, co
     doc = Document()
     doc.add_heading("Codebook Summary Report", level=1)
     df = df.dropna(how='all')  # Remove rows with all NaNs
+    code_df = code_df[~code_df["Type"].astype(str).str.lower().eq("0")]
     columns = code_df["Column"] if code_df is not None else df.columns
     for col in columns:
         if col not in column_types:
